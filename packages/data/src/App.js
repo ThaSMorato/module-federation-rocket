9746a4425d41a796006c342d41f286194110cf05
Thales Silva Morato <44843722+ThaSMorato@users.noreply.github.com>
2021-01-18 10:19:21 -0300

    initial commit

---
+++ packages/data/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AuthProvider from 'auth/AuthProvider'
+
+import Router from './router/Router';
+import PokemonProvider from './providers/PokemonProvider';
+
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <AuthProvider>
+        <PokemonProvider>
+          <Router />
+        </PokemonProvider>
+      </AuthProvider>
+    </>
+  );
+}
+
+export default App;
